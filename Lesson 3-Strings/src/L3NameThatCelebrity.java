0c9613a9e78e87a1817e74225dff6299f5902b7d
cmarsac <cmarsac@HS-D210-12.MOHS.local>
2019-02-28 14:19:32 -0500

    2/28/19

---
+++ Lesson 3-Strings/src/L3NameThatCelebrity.java
@@ -0,0 +1,33 @@
+
+public class L3NameThatCelebrity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Allan Alda";
+		System.out.println(s1.substring(0, 10) + ">>>" + s1.substring(2, 5) + " " + s1.substring(6, 7));
+
+		String s2 = "John Wayne";
+		System.out.println(s2.substring(0, 10) + ">>>" + s2.substring(2, 4) + " " + s2.substring(5, 7));
+
+		String s3 ="Gregory Peck";
+		System.out.println(s3.substring(0,12) + ">>>" + s3.substring(2,7) + " " + s3.substring(8,9));
+
+		
+	
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- Lesson 3-Strings/src/StringsL3.java
+++ Lesson 3-Strings/src/StringsL3.java
@@ -14,6 +14,21 @@ public class StringsL3 {
 		System.out.println("I\"love\"\nbooks");
 		
 		
+		//Lesson 3 Exercise
+		String m ="\"Look here\"";
+		System.out.println(m + "has" + " " + (m.length()-2) + " " + "characters");
+		
+		String n ="\"good\"";
+		System.out.println("All" + " " + n + " " + "men should come to the aid of their country." );
+				
+		String o ="Hello";
+		String p ="Hello Again";
+		System.out.println(o.substring(0,5) + "\n" + p.substring(0,11));
+		
+		String q ="A backslash looks like this \, ...right?";
+		System.out.printlm(q.substring())
+	
+		
 		
 	}
 
