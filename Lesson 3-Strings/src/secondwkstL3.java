5c09d39c01c358bbb3a2ae07434ce32172b0bd4c
cmarsac <cmarsac@HS-D210-12.MOHS.local>
2019-03-08 12:10:49 -0500

    3/8/19

--- Lesson 3-Strings/src/secondwkstL3.java
+++ Lesson 3-Strings/src/secondwkstL3.java
@@ -29,17 +29,27 @@ public class secondwkstL3 {
 		System.out.println(w6.toUpperCase());
 		
 		String w7 ="it's my party";
-		System.out.print(w7.substring(0,8));
-		System.out.println("\"" + w7.substring(8,13).toUpperCase() + "\"");
+		System.out.print(w7.substring(0,8) + "\"" + w7.substring(8,13).toUpperCase() + "\"");
 		
 		String w8 ="whose a whats it";
-		System.out.println(w8.substring(0,5).toUpperCase);
-		System.out.println("is");
-		System.out.println("");
+		System.out.println(w8.substring(0,5).toUpperCase() + w8.substring(14,15) + w8.substring(3,4) + " " + "WHAT");
 		
+		String w9 ="Every Way The Wind Blows";
+		System.out.println(w9.substring(0,5).toLowerCase() + " " + w9.substring(6,9).toUpperCase() + " " + 
+		w9.substring(10,13).toLowerCase() + " " + "\\" + w9.substring(14,18).toUpperCase() + "\\" + " " + 
+		w9.substring(19,24).toLowerCase() + " " + "4");
+	
+		String w10 ="Peter Piper Picked A Peck Of Pickled Peppers";
+		System.out.print(w10.substring(19,20).toUpperCase() + " " + w10.substring(21,37).toLowerCase());
+		System.out.print("             ");
+		System.out.print(w10.substring(37,44).toLowerCase() + " " + w10.substring(0,5) + " " + w10.substring(6,11) + " ");
+		System.out.print("             ");
+		System.out.println(w10.substring(29,36).toLowerCase());
 		
-		
-		
+		String w11 ="word has 4 letters";
+		int w =9;
+		System.out.println("\"" + w11.substring(9,10) + "\"" + " " + w11.substring(11,18) + " " + w11.substring(5,8) + " " + "\""
+		+ w + "\"" + " " + w11.substring(11,18));
 		
 		
 		
