eb5d6cd8ad05aad3050b573f5ada0314093f594b
cmarsac <cmarsac@HS-D210-12.MOHS.local>
2019-03-12 14:16:47 -0400

    3/12/19

--- Lesson 3-Strings/src/secondwkstL3.java
+++ Lesson 3-Strings/src/secondwkstL3.java
@@ -5,34 +5,31 @@ public class secondwkstL3 {
 		// TODO Auto-generated method stub
 
 		//3-6-19
-		//#!
+
 		System.out.println("a\\b\\c\\\"d\"");
-		//#2
+		
 		System.out.println("\\t is a tab, \\n is a new line, \\\" prints a \"quote\"");
-		//#3
+		
 		System.out.println("\"line one\"");
 		System.out.println("\\line         two\\");
-		//#4
+		
 		System.out.println("\\\\denotes a single line comment");
-		System.out.println("\\\\*denotes a multiline comment");
-		//#5
+		System.out.println("\\*denotes a multiline comment*\\");
+		
 		System.out.println("\"\\\\\"\\\"\"");
 		System.out.println("        \\\"\\\"\"\\");
 		System.out.println("\\\"\"\"\"\\");
-		
-		
-		
-		
 		
 	
 		String w6 ="Birthday";
 		System.out.println(w6.toUpperCase());
 		
 		String w7 ="it's my party";
-		System.out.print(w7.substring(0,8) + "\"" + w7.substring(8,13).toUpperCase() + "\"");
+		System.out.println(w7.substring(0,8) + "\"" + w7.substring(8,13).toUpperCase() + "\"");
 		
 		String w8 ="whose a whats it";
-		System.out.println(w8.substring(0,5).toUpperCase() + w8.substring(14,15) + w8.substring(3,4) + " " + "WHAT");
+		System.out.println(w8.substring(0,5).toUpperCase());
+		System.out.println(w8.substring(14,15) + w8.substring(3,4) + " " + w8.substring(8,12));
 		
 		String w9 ="Every Way The Wind Blows";
 		System.out.println(w9.substring(0,5).toLowerCase() + " " + w9.substring(6,9).toUpperCase() + " " + 
@@ -44,15 +41,23 @@ public class secondwkstL3 {
 		System.out.print("             ");
 		System.out.print(w10.substring(37,44).toLowerCase() + " " + w10.substring(0,5) + " " + w10.substring(6,11) + " ");
 		System.out.print("             ");
-		System.out.println(w10.substring(29,36).toLowerCase());
+		System.out.println(w10.substring(12,18).toLowerCase());
 		
 		String w11 ="word has 4 letters";
 		int w =9;
 		System.out.println("\"" + w11.substring(9,10) + "\"" + " " + w11.substring(11,18) + " " + w11.substring(5,8) + " " + "\""
 		+ w + "\"" + " " + w11.substring(11,18));
 		
+		String w12 ="if SUBSTRING is used";
+		int q =9;
+		System.out.println(w12.substring(3,12).toLowerCase() + " " + w12.substring(13,15) + " " + q );
 		
-		
+		String w13 ="Jim, Jacky, Johnny, Jill";
+		int a =3; int b =4; int c =5; int d =6;
+		System.out.println(w13.substring(0,3).toLowerCase() + "\\" + a );
+		System.out.println(w13.substring(5,10).toUpperCase() + "\\" + c);
+		System.out.println(w13.substring(12,18).toLowerCase() + "\\" + d);
+		System.out.println(w13.substring(20,24).toUpperCase() + "\\" + b);
 		
 	}
 
